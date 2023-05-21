4cc4b133bad4e497ac7480bb3a3a888a40638caa
gkoren2 <guy.koren@intel.com>
2023-05-21 08:21:44 +0300

    adding admin files (create env and gitignore)

---
+++ create_p5.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+conda deactivate
+conda activate 
+env_name='p5'
+echo "creating conda env ${env_name}"
+anaconda_path=$CONDA_PREFIX
+echo ${anaconda_path}
+
+source ${anaconda_path}/etc/profile.d/conda.sh
+
+conda deactivate
+conda activate 
+
+echo "Updating conda itself"
+conda update conda -y
+
+
+
+python_version='python=3.9.7'
+
+conda_default_packages='pandas scikit-learn tqdm pyyaml'
+
+anaconda_packages='jupyter pillow h5py scikit-image scikit-learn'
+
+conda_forge_packages='jupyter_contrib_nbextensions matplotlib gensim einops imageio scipy tqdm python-utils pygments'
+
+pip_packages='transformers==4.2.1 sentencepiece tensorboard'
+
+
+
+
+echo "Creating environment and installing Python + Pytorch"
+conda create -y -n $env_name $python_version
+
+echo "Installing pytorch version 1.13 with cuda 11.6 based on the command line from their site"
+conda install -y -n $env_name pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia
+
+echo "Installing default channel packages"
+conda install -y  -n $env_name $conda_default_packages
+
+
+echo "Installing anaconda packages"
+conda install -y -n $env_name -c anaconda $anaconda_packages
+
+echo "Installing conda-forge packages"
+conda install -y -n $env_name -c conda-forge $conda_forge_packages
+
+echo "Installing pytorch packages"
+conda install -y -n $env_name -c pytorch $pytorch_packages
+
+
+echo "Installing pip packages"
+conda activate $env_name
+pip install $pip_packages
+
+echo "at the end, copy the file to ${CONDA_PREFIX}"
+
+
+
+
